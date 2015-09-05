6276e39528b7f5989dc2a87e5e37c8b8dee3ab4e
mzp <mzpppp@gmail.com>
2015-09-05 21:41:19 +0900

    bundle gem prpr-mention_comment

---
+++ lib/prpr/mention_comment.rb
@@ -0,0 +1,7 @@
+require "prpr/mention_comment/version"
+
+module Prpr
+  module MentionComment
+    # Your code goes here...
+  end
+end
---
+++ lib/prpr/mention_comment/version.rb
@@ -0,0 +1,5 @@
+module Prpr
+  module MentionComment
+    VERSION = "0.1.0"
+  end
+end
