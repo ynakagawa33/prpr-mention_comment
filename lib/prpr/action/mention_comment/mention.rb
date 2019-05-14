c794b439685af9f5ca27ad66d30b898ba927e68f
ynakagawa33 <y.nakagawa@sansan.com>
2019-05-15 00:26:21 +0900

    Modified: プラグイン側でブランチを指定するようにして、大本の prpr を変更せずに済むようにした

--- lib/prpr/action/mention_comment/mention.rb
+++ lib/prpr/action/mention_comment/mention.rb
@@ -66,7 +66,7 @@ module Prpr
         end
 
         def config
-          @config ||= Config::Github.new(repository_name)
+          @config ||= Config::Github.new(repository_name, 'develop')
         end
 
         def env
